f774669c308b8a99cc8adc447632abcf5c1c9f04
Nathan Costea <cnlnat@yahoo.com>
2019-07-15 01:15:51 -0400

    Initial commit: Up to a rough start.

---
+++ ResearchAndStudy/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResearchAndStudy
+{
+    class Menu
+    {
+        public string welcome = "Welcome to the Research and Study Project!";
+        public void DisplayMenu()
+        {
+            WelcomeMessage(welcome);
+
+            DisplayOptions();
+        }
+
+        private void WelcomeMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private void DisplayOptions()
+        {
+            Console.WriteLine("Choose an option");
+            Console.WriteLine("1. Calculate the sum of 2 numbers.");
+            Console.WriteLine("2. Calculate the time to a future date.");
+            Console.WriteLine("3. Exit!");
+        }
+
+        public char ReadMenuOption()
+        {
+            return Console.ReadKey().KeyChar;
+        }
+
+        public bool ValidateMenuOption(char option)
+        {
+            // TO DO
+            if (char.IsDigit(option))
+            {
+                var chosen = int.Parse(option.ToString());
+            }
+            return false;
+        }
+    }
+}
---
+++ ResearchAndStudy/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResearchAndStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage("Welcome to the Research and Study Project!");
+
+            ProcessTheRequest();
+        }
+
+        /// <summary>
+        /// Prints a welcome message.
+        /// </summary>
+        /// <param name="message">The welcome message to be printed.</param>
+        private static void WelcomeMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        /// <summary>
+        /// Displays the menu.
+        /// </summary>
+        private static void DisplayTheMenu()
+        {
+            Console.WriteLine("Choose an option");
+            Console.WriteLine("1. Calculate the sum of 2 numbers.");
+            Console.WriteLine("2. Calculate the time to a future date.");
+            Console.WriteLine("3. Exit!");
+        }
+
+        private static void ProcessTheRequest()
+        {
+            // The truth value of the chosen option.
+            int valid;
+            do
+            {
+                // Display the menu.
+                DisplayTheMenu();
+                // Read the option.
+                var option = Console.ReadKey().KeyChar;
+                // Validate the entry.
+                valid = ValidateOption(option);
+                
+            } while (valid);
+        }
+
+        private static bool ValidateOption(char option)
+        {
+            var valid = 0;
+            if (char.IsDigit(option))
+            {
+                va = int.Parse(option.ToString());
+            }
+
+            return false;
+        }
+    }
+}
