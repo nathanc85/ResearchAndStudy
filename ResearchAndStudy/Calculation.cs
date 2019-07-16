44dc7232f7b73c25d64088657b98ac4e6664c4d7
Nathan Costea <cnlnat@yahoo.com>
2019-07-15 21:58:16 -0400

    Improve the results' visibility.

--- ResearchAndStudy/Calculation.cs
+++ ResearchAndStudy/Calculation.cs
@@ -10,13 +10,17 @@ namespace ResearchAndStudy
     {
         public static void CalculateSum(double first, double second)
         {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
             Console.WriteLine($"The sum between {first} and {second} is {first + second}");
+            Console.ResetColor();
         }
 
         public static void CalculateTimeDifference(DateTime futureDate)
         {
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
             TimeSpan timeDifference = futureDate - DateTime.Now;
             Console.WriteLine($"There are {timeDifference.Days} days, {timeDifference.Hours} hours and {timeDifference.Minutes} minutes until {futureDate.ToString()}");
+            Console.ResetColor();
         }
     }
 }
